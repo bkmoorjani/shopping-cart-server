808b49feb309c83425f4178ab64e3893c5fd65ee
bkmoorjani <bkmoorjani@gmail.com>
2021-11-15 19:15:29 -0600

    'createdOrders'

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
+import { CustomersModule } from './customers/customers.module';
 
 @Module({
   
-  imports: [MongooseModule.forRoot('mongodb+srv://mongodb:mongodb@cluster0.k4aq2.mongodb.net/shopping-cart?retryWrites=true&w=majority'),ProductsModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://mongodb:mongodb@cluster0.k4aq2.mongodb.net/shopping-cart?retryWrites=true&w=majority'),ProductsModule, OrdersModule, CustomersModule],
   controllers: [AppController],
   providers: [AppService],
 })
